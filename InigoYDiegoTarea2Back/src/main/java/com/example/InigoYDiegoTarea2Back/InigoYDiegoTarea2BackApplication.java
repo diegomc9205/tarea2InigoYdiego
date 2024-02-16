4bd100885bc41d2582dc8cd97e6a5703a680f046
dam2 <dam2@Mac-Mini-08.local>
2024-02-16 17:57:26 +0100

    Empezamos

---
+++ InigoYDiegoTarea2Back/src/main/java/com/example/InigoYDiegoTarea2Back/InigoYDiegoTarea2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.InigoYDiegoTarea2Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InigoYDiegoTarea2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InigoYDiegoTarea2BackApplication.class, args);
+	}
+
+}
---
+++ InigoYDiegoTarea2Back/src/test/java/com/example/InigoYDiegoTarea2Back/InigoYDiegoTarea2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InigoYDiegoTarea2Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InigoYDiegoTarea2BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
